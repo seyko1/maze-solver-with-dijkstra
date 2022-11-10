3289bd5ec4d5556fdfcf140b2b9d5a8805748ed1
François GODIN <francois.godin@mfa.fr>
2022-11-10 15:58:36 +0100

    improve NextMove() function <3

--- PokemonMaze/Exercises/Exercise6.cs
+++ PokemonMaze/Exercises/Exercise6.cs
@@ -37,30 +37,36 @@ public static class Exercice6
     }
 
     /// <summary>
-    /// calcule la position d’arrivée du prochain mouvement du personnage.
+    /// Calcule la position d’arrivée <see cref="(int, int)"/> du prochain mouvement du personnage.
     /// </summary>
-    /// <param name="array"></param>
-    /// <param name="x"></param>
-    /// <param name="y"></param>
-    /// <param name="direction"></param>
-    /// <returns></returns>
-    public static (int, int) NextMove(Cell[,] array, int x, int y, Direction direction, bool sliding = false)
-    {		
+    /// <param name="array">Le tableau de type <see cref="Cell"/> à analyser.</param>
+    /// <param name="x">La position x actuelle du personnage.</param>
+    /// <param name="y">La position y actuelle du personnage.</param>
+    /// <param name="direction">La direction de type <see cref="Direction"/> du prochain mouvement du personnage.</param>
+    /// <returns>La position d'arrivée <see cref="(int, int)"/> du personnage</returns>
+    /// <see cref="NextMove(Cell[,] array, int x, int y, Direction direction, bool sliding)"/>
+    public static (int, int) NextMove(Cell[,] array, int x, int y, Direction direction)
+    {
+        // Ici, nous exposons volontairement NextMove() en public pour masquer le paramètre sliding.
+        // C'est une fonction "vide" qui se contente d'appeler la fonction récursive avec un paramètre d'initialisation pour l'accumulateur sliding.
+        return NextMove(array, x , y, direction, false);
+    }
+
+    // Fonction récursive utilisant l'accumulateur sliding pour gérer le cas où le personnage passe sur une case glissante du labyrinthe.
+    private static (int, int) NextMove(Cell[,] array, int x, int y, Direction direction, bool sliding)
+    {
         (int nextY, int nextX) = NextPosition(x, y, direction);
+        
         if (IsValid(array, nextX, nextY))
         {
             var currentCell = array[y, x];
             var nextCell    = array[nextY, nextX];
 
-            if (nextCell == Cell.Snow)
+            if (nextCell == Cell.Snow || (nextCell == Cell.Grass && !sliding))
             {
                 (nextY, nextX) = NextMove(array, nextX, nextY, direction, true);
             }
-            
-            if (nextCell == Cell.Grass && (!sliding || (currentCell != Cell.Snow && currentCell != Cell.Grass)))
-            {
-                (nextY, nextX) = NextMove(array, nextX, nextY, direction, true);
-            }
+
             return (nextY, nextX);
         }
         return (y, x);
